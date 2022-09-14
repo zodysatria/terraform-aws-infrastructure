ef956291e3b04cf73af54a11289547b0d5875699
zody satria <zodysatria@gmail.com>
2022-09-14 11:13:42 +0700

    feat(prometheus): add new ec2 for prometheus server

---
+++ monitoring/prometheus/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  version = "~> 3.0"
+  region  = "ap-southeast-3"
+  profile = "zodynoc1"
+}
+
+module "ec2" {
+  source = "git@github.com:zodysatria/terraform-aws-modules-ec2.git?ref=v0.0.1"
+
+  name                        = "prometheus"
+  project                     = "terraform"
+  environment                 = "development"
+  ami                         = "ami-00c5331644ad576ad" #Ubuntu20
+  instance_type               = "t3.micro"
+  vpc_name                    = "vpc-example-terraform"
+  associate_public_ip_address = true
+  volume_size                 = 8
+  user_data_file              = "setup.sh"
+
+  #  security_groups_sg_id = [
+  #    {
+  #      from_port                = 22,
+  #      to_port                  = 22,
+  #      protocol                 = "tcp"
+  #      source_security_group_id = "sg-0c1011e9e5e6ec8e6"
+  #      description              = "example add sg 1"
+  #    },
+  #    {
+  #      from_port                = 80,
+  #      to_port                  = 80,
+  #      protocol                 = "tcp"
+  #      source_security_group_id = "sg-0c1011e9e5e6ec8e6"
+  #      description              = "example add sg 2"
+  #    },
+  #  ]
+
+  security_groups_cidr = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+      description = "Terraform poc add sg 1"
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+      description = "Terraform poc add sg 2"
+    },
+  ]
+}
